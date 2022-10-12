53766fa27b36e15d3fe29e614147e37d45041b46
witchicken <witchicken2@gmail.com>
2022-10-12 15:49:21 +0900

    slideend

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-keyboardEvent = 방향키 입력시 box이동
-slider = slide모듈화
+1.keyboardEvent = 방향키 입력시 box이동
+
+2.slider = slide모듈화
+기본 사용법
+
+  <div class="enigSlider" width="???" height="???" boxShadow="???">
+      <div class="enigSlider_wrapper">
+        <Contents>
+        <Contents>
+        <Contents>
+        <Contents>
+      </div>
+    </div>
+    <script src="./chickenSlider.js"></script>
--- module/slider/chickenSlider.js
+++ module/slider/chickenSlider.js
@@ -1,7 +1,7 @@
 (function () {
   const container = document.querySelector(".enigSlider");
   class EnigSlider {
-    constructor(width, height) {
+    constructor(width, height, boxShadow) {
       this.width = width;
       this.height = height;
       this.container = document.querySelector(".enigSlider");
@@ -9,13 +9,18 @@
       this.margin = this.width / 25;
       this.length = this.viewer.children.length;
       this.viewIndex = this.viewer.children.length;
+      this.realChild = [
+        ...document.querySelector(".enigSlider_wrapper").children,
+      ];
+      this.boxShadow = boxShadow;
     }
     makeContainer() {
       //const inner = this.container.children[0].children;
       this.container.style.width = this.width + "px";
       this.container.style.height = this.height + "px";
       this.container.style.position = "relative";
-      //this.container.style.overflow = "hidden";
+      this.container.style.overflow = "hidden";
+      this.container.style.boxShadow = this.boxShadow;
     }
     makeViewer() {
       this.viewer.style.height = this.height + "px";
@@ -27,23 +32,26 @@
     makeDirection() {
       const leftBtn = document.createElement("div");
       const rightBtn = document.createElement("div");
-      leftBtn.style.width = this.height / 20 + "px";
-      leftBtn.style.height = this.height / 20 + "px";
+      leftBtn.classList.add("leftBtn");
+      rightBtn.classList.add("rightBtn");
+
+      leftBtn.style.width = this.margin / 1.5 + "px";
+      leftBtn.style.height = this.margin / 1.5 + "px";
       leftBtn.style.position = "absolute";
       leftBtn.style.borderTop = "2px solid black";
       leftBtn.style.borderRight = "2px solid black";
-      leftBtn.style.left = "2px";
+      leftBtn.style.left = this.margin / 3 + "px";
       leftBtn.style.top = "50%";
       leftBtn.style.transform = "translateY(-50%) rotate(-135deg)";
       leftBtn.style.cursor = "pointer";
       leftBtn.style.zIndex = 99;
       //rightBtn
-      rightBtn.style.width = this.height / 20 + "px";
-      rightBtn.style.height = this.height / 20 + "px";
+      rightBtn.style.width = this.margin / 1.5 + "px";
+      rightBtn.style.height = this.margin / 1.5 + "px";
       rightBtn.style.position = "absolute";
       rightBtn.style.borderTop = "2px solid black";
       rightBtn.style.borderRight = "2px solid black";
-      rightBtn.style.right = "2px";
+      rightBtn.style.right = this.margin / 3 + "px";
       rightBtn.style.top = "50%";
       rightBtn.style.transform = "translateY(-50%) rotate(45deg)";
       rightBtn.style.cursor = "pointer";
@@ -54,8 +62,6 @@
     }
 
     makeList() {
-      const listCount = this.viewer.children.length;
-      let left = 0;
       const realChild = [...this.viewer.children];
       realChild.forEach((item) => {
         item.style.width = `${Number(this.width) - Number(this.margin) * 2}px`;
@@ -65,12 +71,12 @@
         item.style.padding = this.margin + "px";
       });
     }
-    appendPrevClone() {
-      const length = this.length;
-      const realChild = [...this.viewer.children];
+    appendPrevClone(cls) {
+      const length = this.length; //7
+      const realChild = this.realChild;
       for (let i = length - 1; i >= 0; i--) {
         const cloneSlide = realChild[i].cloneNode(true);
-
+        cloneSlide.classList.add(cls);
         this.viewer.prepend(cloneSlide);
       }
     }
@@ -86,46 +92,88 @@
       const leftBtn = this.container.children[0];
       const rightBtn =
         this.container.children[this.container.children.length - 1];
-      const realChild = [...this.container.children].splice(
-        1,
-        this.container.children.length - 2
+
+      //debounce
+      const debounce = (callback, delay) => {
+        let timerId;
+        return (...args) => {
+          //delay가 경과하기 이전에 이벤트가 발생하면 이전 이벤트를 취소하고 새로운 타이머를 재설정한다.
+          if (timerId) clearTimeout(timerId);
+          timerId = setTimeout(callback, delay, ...args);
+        };
+      };
+      leftBtn.addEventListener(
+        "click",
+        debounce(() => {
+          this.viewer.style.left =
+            Number(this.viewer.style.left.replace("px", "")) +
+            Number(this.width) +
+            "px";
+          this.viewIndex--;
+          if (this.viewIndex % this.length == 0) {
+            try {
+              setTimeout(() => {
+                this.viewer.style.transition = "";
+                this.viewer.style.left = -this.length * this.width + "px";
+                this.viewer.style.left;
+                this.viewIndex = this.length;
+              }, 500);
+              setTimeout(() => {
+                this.viewer.style.transition = "left .5s linear";
+              }, 600);
+            } catch (error) {
+            } finally {
+            }
+          }
+        }, 300)
       );
-      leftBtn.addEventListener("click", () => {
-        console.log("leftClicked");
-        console.log(this.viewer.style.left);
-        this.viewer.style.left =
-          Number(this.viewer.style.left.replace("px", "")) +
-          Number(this.width) +
-          "px";
-        this.viewIndex--;
-        console.log(this.viewIndex);
-        console.log(this.viewIndex % this.length);
-        if (this.viewIndex % this.length == 0) {
-          console.log("test");
-          this.appendPrevClone();
-          console.log(this);
-          console.log(this.appendPrevClone());
-          console.log(enigSlider.appendPrevClone());
-          //내부객체 메소드 접근문제
-          //addEvent를 외부로 뺼까?
-        }
-      });
-      rightBtn.addEventListener("click", () => {
-        console.log("rightClicked");
-      });
+
+      rightBtn.addEventListener(
+        "click",
+        debounce(() => {
+          this.viewer.style.left =
+            Number(this.viewer.style.left.replace("px", "")) -
+            Number(this.width) +
+            "px";
+          this.viewIndex--;
+          if (this.viewIndex % this.length == 0) {
+            try {
+              setTimeout(() => {
+                this.viewer.style.transition = "";
+                this.viewer.style.left = -this.length * this.width + "px";
+                this.viewer.style.left;
+                this.viewIndex = this.length;
+              }, 500);
+              setTimeout(() => {
+                this.viewer.style.transition = "left .5s linear";
+              }, 600);
+            } catch (error) {
+            } finally {
+            }
+          }
+        }, 300)
+      );
     }
   }
   let defaultWidth = 200;
   let defaultHeight = 200;
+  let defaultBoxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
   if (container.attributes[1]) {
     defaultWidth = container.attributes[1].value;
   }
   if (container.attributes[2]) {
     defaultHeight = container.attributes[2].value;
   }
-  const enigSlider = new EnigSlider(defaultWidth, defaultHeight);
+  if (container.attributes[3]) {
+    defaultBoxShadow = container.attributes[3].value;
+  }
+  const enigSlider = new EnigSlider(
+    defaultWidth,
+    defaultHeight,
+    defaultBoxShadow
+  );
   enigSlider.makeContainer();
-  enigSlider.appendPrevClone();
+  enigSlider.appendPrevClone("12");
   enigSlider.appendAfterClone();
   enigSlider.makeViewer();
   enigSlider.makeDirection();
